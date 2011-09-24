14907c89065a5f1929345cfee3c9945cfd031f71
Roman Kharitonov <refaim.vl@gmail.com>
2011-09-24 20:14:10 +1100

    Restore task numeration

--- t03.c
+++ t03.c
@@ -8,16 +8,18 @@
 #define N 120
 
 int gCounter = 0;
+int gOther = 0;
+int gbPrinted = 1;
 
 void* first_body(void* arg)
 {
     for (int i = 2; i <= N; i += 2)
     {
-        while (gCounter % 2 == 0)
+        while ((gbPrinted == 0) || (gCounter % 2 == 0))
         {
             sleep(1);
         }
-        printf("%d\n", i);
+        gbPrinted = 0;
         gCounter = i;
     }
 }
@@ -26,21 +28,36 @@ void* second_body(void *arg)
 {
     for (int i = 1; i <= N + 1; i += 2)
     {
-        while (gCounter % 2 != 0)
+        while ((gbPrinted == 0) || (gCounter % 2 != 0))
         {
             sleep(1);
         }
-        printf("%d\n", i);
+        gbPrinted = 0;
         gCounter = i;
+    }
+}
+
+void* third_body(void* arg)
+{
+    while (gCounter <= N+1)
+    {
+        if ((gCounter != 0) && (gOther != gCounter))
+        {
+            gOther = gCounter;
+            printf("%d\n", gOther);
+            gbPrinted = 1;
+        }
     }
 }
 
 int main()
 {
-    pthread_t first, second;
+    pthread_t first, second, third;
 
     PThreadCall(pthread_create(&first, NULL, first_body, NULL))
     PThreadCall(pthread_create(&second, NULL, second_body, NULL))
+    PThreadCall(pthread_create(&third, NULL, third_body, NULL))
     PThreadCall(pthread_join(first, NULL))
     PThreadCall(pthread_join(second, NULL))
+    PThreadCall(pthread_join(third, NULL))
 }
