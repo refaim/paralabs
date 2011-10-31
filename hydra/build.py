05ec8027b97a8788af0c51d823017f912735050b
Roman Kharitonov <refaim.vl@gmail.com>
2011-11-01 07:48:18 +1100

    Hydra: Fix build.py for linux

--- hydra/build.py
+++ hydra/build.py
@@ -6,8 +6,10 @@ import shlex
 if __name__ == '__main__':
     if platform.system() == 'Windows':
         make = 'mingw32-make -fMakefile.mingw'
+        shell = True
     else:
         make = 'make'
+        shell = False
     cmd = shlex.split(make) + sys.argv[1:]
     print ' '.join(cmd)
-    subprocess.call(cmd, shell=True)
+    subprocess.call(cmd, shell=shell)
