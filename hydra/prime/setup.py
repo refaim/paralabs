069bfa3e62b94596ff69ca0c2f3cc69283fedf25
Roman Kharitonov <refaim.vl@gmail.com>
2011-11-15 21:12:58 +1100

    Hydra: Enable -O2 by default

--- hydra/prime/setup.py
+++ hydra/prime/setup.py
@@ -3,7 +3,7 @@
 module = Extension(
     'prime',
     sources=['primemodule.c', 'myrand.c'],
-    extra_compile_args=['-std=c99', '-Wextra', '-pedantic', '-Wno-unused'],
+    extra_compile_args=['-std=c99', '-Wextra', '-pedantic', '-Wno-unused', '-O2'],
 )
 
 setup(name='Prime', ext_modules=[module])
