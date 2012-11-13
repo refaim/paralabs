84cf6331029cd63338cdb0f366b3cf3005433fb5
Roman Kharitonov <refaim.vl@gmail.com>
2012-11-12 19:01:07 -0500

    UselessShell: Support pipes

--- 2012/os/ush.py
+++ 2012/os/ush.py
@@ -46,33 +46,52 @@ class UselessShell(cmd.Cmd):
         def environ_replacer(match):
             return self.environ.get(match.groups()[0], '')
 
+        def split_command(line, char):
+            commands = []
+            cmd = line
+            amp = line.find(char)
+            while amp != -1:
+                cmd = line[:amp]
+                line = line[amp + 1:]
+                commands.append((cmd, True))
+                amp = line.find(char)
+                cmd = line
+            if cmd:
+                commands.append((cmd, False))
+            return commands
+
         line = line.strip()
         for key, value in sorted(self.environ.iteritems()):
             line = line.replace('$' + key, value)
 
-        commands = []
-        cmd = line
-        amp = line.find('&')
-        while amp != -1:
-            cmd = line[:amp]
-            line = line[amp + 1:]
-            commands.append((cmd, True))
-            amp = line.find('&')
-            cmd = line
-        if cmd:
-            commands.append((cmd, False))
-
-        for command, background in commands:
-            args = [arg for arg in command.split(' ') if arg.strip()]
-            try:
-                process = subprocess.Popen(args, env=self.environ)
+        pipes = split_command(line, '|')
+        is_prev_piped = False
+        for pidx, (rawcmd, is_piped) in enumerate(pipes):
+            commands = split_command(rawcmd, '&')
+            for cidx, (command, background) in enumerate(commands):
+                if is_prev_piped and pidx > 0 and cidx == 0:
+                    stdin = stdout
+                else:
+                    stdin = None
+                if is_piped and cidx == len(commands) - 1:
+                    background = True
+                    stdout = subprocess.PIPE
+                else:
+                    stdout = None
+                args = [arg for arg in command.split(' ') if arg.strip()]
+                try:
+                    process = subprocess.Popen(args, env=self.environ,
+                        stdin=stdin, stdout=stdout)
+                except OSError, ex:
+                    print ex.strerror
+                    continue
+                stdout = process.stdout
                 self.processes.append(process)
                 self.lastcmd = ''
                 if not background:
                     process.wait()
                     self.processes.pop()
-            except OSError, ex:
-                print ex.strerror
+                is_prev_piped = is_piped
 
 
 def main(args):
